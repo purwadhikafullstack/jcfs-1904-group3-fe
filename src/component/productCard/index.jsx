80f241ac58ea388f76ef6c2cdc9e58435020813e
FahemHakikiKhaya <hakikihaya@gmail.com>
2022-03-29 15:25:32 +0700

    Updated Feature: Product Card

--- src/component/productCard/index.jsx
+++ src/component/productCard/index.jsx
@@ -3,26 +3,28 @@ import { Card, Button } from "react-bootstrap";
 import "./style.css";
 function Index() {
   return (
-    <Card style={{ width: 400, height: 300, border: 0 }}>
-      <div style={{ width: 400, height: 300, border: 0 }}>
-        <Card.Img
-          variant="top"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnajNWhJQ6fD4jNMVub7y0Wfep4ZwYzeIQYA&usqp=CAU"
-          className="image"
-        />
-      </div>
-      <Card.Body
-        style={{
-          padding: 0,
-          display: "flex",
-          justifyContent: "space-between",
-        }}
-      >
-        <Card.Text>Product Name</Card.Text>
-        <Card.Text>$Rp.50000</Card.Text>
-      </Card.Body>
-      <Button variant="outline-dark">Dark</Button>
-    </Card>
+    <div style={{ width: 400, height: 300, border: 0 }} className="card">
+      <Card>
+        <div style={{ width: 400, height: 300, border: 0 }}>
+          <Card.Img
+            variant="top"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnajNWhJQ6fD4jNMVub7y0Wfep4ZwYzeIQYA&usqp=CAU"
+            className="image"
+          />
+        </div>
+        <Card.Body
+          style={{
+            padding: 0,
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <Card.Text>Product Name</Card.Text>
+          <Card.Text>$Rp.50000</Card.Text>
+        </Card.Body>
+        <Button variant="outline-dark">Dark</Button>
+      </Card>
+    </div>
   );
 }
 
