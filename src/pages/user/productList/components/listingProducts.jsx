5aa21827e6c6385be18e0be68278166b469a783a
FahemHakikiKhaya <hakikihaya@gmail.com>
2022-04-01 15:38:47 +0700

    new feature : sorting products

--- src/pages/user/productList/components/listingProducts.jsx
+++ src/pages/user/productList/components/listingProducts.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ProductCard from "../../../../component/productCard";
 
 function listingProducts(props) {
-  const { products } = props;
+  const { products, paginationState } = props;
+  const { page, itemsPerPage } = paginationState;
 
   const renderProducts = () => {
-    console.log(products);
-    return products.map((product) => <ProductCard product={product} />);
+    const startIndex = (page - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const slicedProducts = products.slice(startIndex, endIndex);
+    return slicedProducts.map((product) => <ProductCard product={product} />);
   };
   return (
     <div className="col-12 " style={{ display: "flex", flexWrap: "wrap" }}>
--- src/pages/user/productList/index.jsx
+++ src/pages/user/productList/index.jsx
@@ -8,13 +8,13 @@ import ListingProducts from "./components/listingProducts";
 
 function Index() {
   const [products, setProducts] = useState([]);
-  const [filteredProducts, setFilteredProducts] = useState([]);
   const [sortedProducts, setSortedProducts] = useState([]);
   const [paginationState, setPaginationState] = useState({
     page: 1,
-    lastPage: 0,
-    itemsPerPage: 5,
+    maxPage: 0,
+    itemsPerPage: 9,
   });
+  const { page, maxPage, itemsPerPage } = paginationState;
   const [age, setAge] = useState("");
 
   const handleChange = (event) => {
@@ -26,14 +26,72 @@ function Index() {
       const res = await axios.get("/products");
       const { data } = res;
       setProducts(data.result);
+      setSortedProducts(data.result);
+      setPaginationState({
+        ...paginationState,
+        maxPage: Math.ceil(data.result.length / paginationState.itemsPerPage),
+      });
     } catch (error) {
       console.log({ error });
     }
   };
 
+  const sortProducts = (e) => {
+    const sortValue = e.target.value;
+    const rawData = [...products];
+
+    switch (sortValue) {
+      case "default":
+        break;
+      case "lowPrice":
+        rawData.sort((a, b) => a.price - b.price);
+        break;
+      case "highPrice":
+        rawData.sort((a, b) => b.price - a.price);
+        break;
+      case "az":
+        rawData.sort((a, b) => {
+          // a : Kaos
+          // b : Celana
+          // b --> a
+
+          if (a.productName < b.productName) {
+            return -1;
+          } else if (a.productName > b.productName) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+        break;
+      case "za":
+        rawData.sort((a, b) => {
+          if (a.productName < b.productName) {
+            return 1;
+          } else if (a.productName > b.productName) {
+            return -1;
+          } else {
+            return 0;
+          }
+        });
+        break;
+    }
+
+    setSortedProducts(rawData);
+  };
+
   useEffect(() => {
     fetchProducts();
+    const rawData = [...products];
+    console.log(products);
   }, []);
+
+  const btnPrevPageHandler = () => {
+    setPaginationState({ ...paginationState, page: page - 1 });
+  };
+  const btnNextPageHandler = () => {
+    setPaginationState({ ...paginationState, page: page + 1 });
+  };
 
   return (
     <div
@@ -49,19 +107,37 @@ function Index() {
       <div>
         <FormControl style={{ width: 220 }}>
           <InputLabel id="demo-simple-select-label">SortBy</InputLabel>
-          <Select value={age} label="SortBy" onChange={handleChange}>
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
+          <Select value={age} label="SortBy" onChange={sortProducts}>
+            <MenuItem value="default">Default</MenuItem>
+            <MenuItem value="lowPrice">Lowest Price</MenuItem>
+            <MenuItem value="highPrice">Highest Price</MenuItem>
+            <MenuItem value="az">A-Z</MenuItem>
+            <MenuItem value="za">Z-A</MenuItem>
           </Select>
         </FormControl>
       </div>
       <div>
-        {products.length ? (
-          <ListingProducts products={products} />
+        {sortedProducts.length ? (
+          <ListingProducts
+            products={sortedProducts}
+            paginationState={paginationState}
+          />
         ) : (
           <h1>Loading......</h1>
         )}
+      </div>
+      <div style={{ marginTop: 50 }}>
+        <div className="d-flex flex-row justify-content-between align-items-center">
+          <button onClick={btnPrevPageHandler} className={`btn btn-dark mr-5 `}>
+            {"<"}
+          </button>
+          <div className="text-center">
+            Page {page} of {maxPage}
+          </div>
+          <button onClick={btnNextPageHandler} className={`btn btn-dark ml-5 `}>
+            {">"}
+          </button>
+        </div>
       </div>
     </div>
   );
